99f7cbaf32f25fe925c42215e46b6b7b6bc189e4
Erica Girges <ericafiorenza@gmail.com>
2020-11-17 09:13:18 -0800

    fixed user endpoints

--- users/users-router.js
+++ users/users-router.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
-
+const bcryptjs = require("bcryptjs");
 const Users = require("./users-model.js");
 const restricted = require("../auth/auth-middleware.js");
-const e = require("express");
 
 router.get("/", restricted, (req, res) => {
     Users.find()
@@ -11,15 +10,38 @@ router.get("/", restricted, (req, res) => {
         }) .catch(err => res.send(err));
 });
 
-router.get("/:id", restricted, (req, res) => {
-    Users.findById(req.id)
+router.get("/:userId", restricted, (req, res) => {
+    Users.findById(req.jwt.userId)
     .then(user => {
         if(!user){
             res.status(404).json({ message: "Can't find user with provided id." })
         } else{
-            res.status(200).json({ user, jwt:req.jwt })
+            res.status(200).json({ user })
         }
-    }) .catch(err => res.status(500).json({ err: "err.message" }))
+    }) .catch(error => {
+        console.log("ERROR", error)
+        res.status(500).json({ error: "Unable to fetch user at this time." })
+    })
+});
+
+router.put("/:id", restricted, (req, res) => {
+    const updates = req.body;
+    if(req.body.password){
+        const rounds = process.env.BCRYPT_ROUNDS || 8;
+        const hash = bcryptjs.hashSync(updates.password, rounds);
+        updates.password = hash
+    }
+    Users.update(req.params.id, updates)
+    .then(user => {
+        if(!user){
+            res.status(404).json({ message: "Can't find user with provided id." })
+        } else{
+            res.status(200).json(user)
+        }
+    }) .catch(error => {
+        console.log("ERROR", error)
+        res.status(500).json({ error: "Unable to edit user info at this time." })
+})
 })
 
 module.exports = router;
